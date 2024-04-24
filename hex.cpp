e075b96f45880dca41f6206f688659b557a0a368
giemat <mat.giedroyc@gmail.com>
2024-04-24 20:49:12 +0200

    project init

---
+++ hex.cpp
@@ -0,0 +1,5 @@
+//
+// Created by matgi on 4/24/2024.
+//
+
+#include "hex.h"
---
+++ hex.h
@@ -0,0 +1,14 @@
+//
+// Created by matgi on 4/24/2024.
+//
+
+#ifndef HEX_HEX_H
+#define HEX_HEX_H
+
+
+class hex {
+
+};
+
+
+#endif //HEX_HEX_H
